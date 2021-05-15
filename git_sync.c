b19302f53b3abda16a1cdf1a81d7a3f2793fce26
mike <moshea730@gmail.com>
2021-05-15 09:29:50 +0100

    Watches a folder continually and updates the dictionary in git when they happen.

--- git_sync.c
+++ git_sync.c
@@ -4,6 +4,7 @@
 sudo apt install libgit2-1.1
 sudo apt install libgit2-dev
 
+gcc git_sync.c -o bin/git_sync
 
 I got this from:
 https://www.thegeekstuff.com/2010/04/inotify-c-program-example/
@@ -23,67 +24,77 @@ https://linuxhint.com/inotify_api_c_languageysi
 
 const char *folder_path = "/home/mike/.vim_spell";
 
+void watch_local_changes(const char *path)
+{
+
+
+}
+
 int main( )
 {
-  int length, i = 0;
-  int fd;
-  int wd;
-  char buffer[EVENT_BUF_LEN];
-
+  // Check the folder exists
   DIR* dir = opendir(folder_path);
   if (dir) { // Directory exists. */
     closedir(dir);
     printf("The folder: '%s' exists.\n", folder_path);
+    watch_local_changes(folder_path);
   } else if (ENOENT == errno) {
     printf("The folder: '%s' does not exist.\n", folder_path);
 	return -1;
-    /* Directory does not exist. */
   } else {
     printf("The folder: '%s' can't be accessed.\n", folder_path);
-	return -2; 
-    /* opendir() failed for some other reason. */
+	return -2; // Can't access folder for some other reason
   }
 
-  /*creating the INOTIFY instance*/
-  fd = inotify_init();
-
-  /*checking for error*/
-  if ( fd < 0 ) {
-    perror( "inotify_init" );
-  }
   do {
-      /*adding the “/tmp” directory into watch list. Here, the suggestion is to validate the existence of the directory before adding into monitoring list.*/
+      int length, i = 0;
+      int fd;
+      int wd;
+      char buffer[EVENT_BUF_LEN];
+
+      printf("\nStarted infinite loop\n");
+      // creating an INOTIFY instance
+      fd = inotify_init();
+
+      // check for errors
+      if ( fd < 0 ) {
+          perror( "inotify_init" );
+      }
+      // Adding the folder to be watch to the watch list. 
       wd = inotify_add_watch( fd, folder_path, IN_CREATE | IN_DELETE | IN_MODIFY );
 
-      /*read to determine the event change happens on “/tmp” directory. Actually this read blocks until the change event occurs*/ 
-
-      length = read( fd, buffer, EVENT_BUF_LEN ); 
+      // Read to until a change event happens in the directory, it blocks until something happens. 
+      length = read(fd, buffer, EVENT_BUF_LEN); 
+      printf("\nHave read buffer\n");
 
       /*checking for error*/
-      if ( length < 0 ) {
+      if (length < 0) {
           perror( "read" );
       }  
 
+      printf("\nBefore Inner loop - length: %d - i: %d\n", length, i);
       /*actually read return the list of change events happens. Here, read the change event one by one and process it accordingly.*/
-      while ( i < length ) {     
+      while (i < length) {    
+	     printf("\nstart of loop - lenght: %d - i: %d\n", length, i); 
           struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];
-          if ( event->len ) {
+          if (event->len) {
       	    if(event->mask & IN_MODIFY) {
 		printf("\n\tFile changed: %s.\n", event->name);
-		char command[512];
+		char command[(58 + strlen(folder_path))];
 		strcpy(command, "cd ");
 		strcat(command, folder_path);
-		strcat(command, " && git add . && git commit -m 'updated' && git push ");
+		strcat(command, " && git add . && git commit -m 'updated' && git push &");
 
 		system(command); 
+		printf("\nExecuted git command\n");
       	    }
    	    i += EVENT_SIZE + event->len;
           }
-          inotify_rm_watch( fd, wd ); // remove directory from watch list
+	  printf("\nStill in inner loop - i: %d\n", i);
+      }
+      inotify_rm_watch(fd, wd); // remove directory from watch list
+      close(fd); // close the INOTIFY
 
-          close( fd ); // close the INOTIFY
-      } 
-  }  while(1); // loop continues
-
-return(0);
+  }  while(1); // loop that doesn't end naturally 
+  return(0);
 }
